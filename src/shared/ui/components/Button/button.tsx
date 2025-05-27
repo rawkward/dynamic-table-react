625fd4c09fd9dc9b21a1e6ceb6c22fc9cacc4688
Anton Andreev <aaandreev93@gmail.com>
2025-05-27 15:58:22 +0300

    set up basic file structure

--- src/shared/ui/components/Button/button.tsx
+++ src/shared/ui/components/Button/button.tsx
@@ -3,7 +3,7 @@ import { Slot } from "@radix-ui/react-slot";
 import { type VariantProps } from "class-variance-authority";
 import { buttonVariants } from "./button-variants.ts";
 
-import { cn } from "@/lib/utils.ts";
+import { cn } from "@/shared/lib/utils.ts";
 
 function Button({
   className,
